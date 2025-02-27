0b86e17567a76899aba9facf93be0dc522694f1a
yudo417 <hayashi.kazuki.hk@gmail.com>
2025-02-28 01:50:10 +0900

    before

--- ProControlerForMac/View/ContentView.swift
+++ ProControlerForMac/View/ContentView.swift
@@ -17,14 +17,23 @@ struct ContentView: View {
             Text("Button A: \(controllerMonitor.buttonA ? "Pressed" : "Released")")
             Text("Left Stick: X = \(controllerMonitor.leftStick.x), Y = \(controllerMonitor.leftStick.y)")
         }
-        .onChange(of:[controllerMonitor.leftStick.x,controllerMonitor.leftStick.y,Float(Nowposition.x),Float(Nowposition.y)]){newValue in
-            let deltaX = newValue[0] * 5.0 // 感度調整（必要に応じて変更）
-            let deltaY = -newValue[1] * 5.0 // Y軸は反転させる（画面座標系に合わせる）
+
+        .onReceive(Timer.publish(every: 1/200, on: .main, in: .common).autoconnect()) { _ in
+            let deltaX = controllerMonitor.leftStick.x * 2
+            let deltaY = -controllerMonitor.leftStick.y * 2
             ccvm.moveCursor(deltaX: deltaX, deltaY: deltaY)
-            print("X:"+(deltaX > 0 ? " " : "")+String(format: "%0.8f", deltaX)+" Y:"+(deltaY >= 0 ? " " : "")+String(format: "%0.8f", deltaY) + " "+String(format: "%0.8f", Nowposition.x)+" "+String(format: "%0.8f", Nowposition.y))
-
-
+            ccvm.pressbuttonA(isAPressed: controllerMonitor.buttonA)
+//            print("X:"+(deltaX > 0 ? " " : "")+String(format: "%0.8f", deltaX)+" Y:"+(deltaY >= 0 ? " " : "")+String(format: "%0.8f", deltaY) + " "+String(format: "%0.8f", Nowposition.x)+" "+String(format: "%0.8f", Nowposition.y))
         }
+
+//        .onChange(of:[controllerMonitor.leftStick.x,controllerMonitor.leftStick.y,Float(Nowposition.x),Float(Nowposition.y)]){newValue in
+//            let deltaX = newValue[0] * 5.0
+//            let deltaY = -newValue[1] * 5.0 // Y軸は反転させる（画面座標系に合わせる）
+//            ccvm.moveCursor(deltaX: deltaX, deltaY: deltaY)
+//            print("X:"+(deltaX > 0 ? " " : "")+String(format: "%0.8f", deltaX)+" Y:"+(deltaY >= 0 ? " " : "")+String(format: "%0.8f", deltaY) + " "+String(format: "%0.8f", Nowposition.x)+" "+String(format: "%0.8f", Nowposition.y))
+//
+//
+//        }
         .padding()
     }
 }
@@ -51,6 +60,13 @@ class CursorController {
         if let moveEvent = CGEvent(mouseEventSource: nil, mouseType: .mouseMoved, mouseCursorPosition: CGPoint(x: newX, y: newY), mouseButton: .left) {
             moveEvent.post(tap: .cghidEventTap)
         }
+    }
+
+    func pressbuttonA(isAPressed:Bool){
+        if let buttonAEvent = CGEvent(mouseEventSource: nil, mouseType: .leftMouseDown, mouseCursorPosition: getPosition(), mouseButton: .left){
+                buttonAEvent.post(tap: .cghidEventTap)
+            }
+
     }
 
     func getPositionString() -> String{
